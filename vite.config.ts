11cd2517355e7b282f07a7f33b000534b9432180
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-06-12 00:32:49 -0500

    prepare for gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 // uncomment base if you want to deploy to github pages
 export default defineConfig({
-	// base: "/dishandle/",
+	base: "/dishandle/",
 	plugins: [react()],
 });
